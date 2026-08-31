985f7ab19d1380ea4bf3360e86bd12e4479cadd9
Mahima kumari Javvadi <mahimakumarijavvadi3@gmail.com>
2026-08-31 23:04:14 +0530

    Update 05_visualization.R

--- R/05_visualization.R
+++ R/05_visualization.R
@@ -1,7 +1,7 @@
 # 05_visualization.R
 # Advertising campaign visualizations
 
-output_dir <- "outputs"
+output_dir <- "output"
 plot_dir <- file.path(output_dir, "plots")
 
 if (!dir.exists(plot_dir)) {
